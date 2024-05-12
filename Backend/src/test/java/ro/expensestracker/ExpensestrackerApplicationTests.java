c34fbf2a08680f92b5855ba333f2d2812d39fd30
Dumitrascu Luca <dumitrasculuca07@gmail.com>
2024-05-12 15:16:21 +0300

    Added Spring Boot to the application with required dependencies

---
+++ Backend/src/test/java/ro/expensestracker/ExpensestrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.expensestracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensestrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
